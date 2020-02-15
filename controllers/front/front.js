6a1b0f01db0044aea07bfffbd7149ccff0f7169b
hcyhehe <960696163@qq.com>
2020-02-16 00:28:24 +0800

    卍解

--- controllers/front/front.js
+++ controllers/front/front.js
@@ -39,15 +39,19 @@ exports.article = async function (req, res, next) {
         let sql = ' select * from article where id = \'' +id+ '\' '
         let [[raw]] = await conn.query(sql)
         raw.create_time = moment(raw.create_time).format('YYYY-MM-DD HH:mm:ss')
-        raw.content = tool.delHtmlTag(unescape(raw.content))
+        raw.content = unescape(raw.content)
+        raw.tags = raw.tags.split(',')
         let sql2 = ' select * from article where is_deleted = 1 order by watch_num desc '
         let [raw2] = await conn.query(sql2)
         raw2.map(item=>{
             item.create_time = moment(item.create_time).format('YYYY-MM-DD')
             item.content = tool.delHtmlTag(unescape(item.content))
         })
+        //增加文章访问量
+        let sql3 = ' update article set watch_num = watch_num + 1 where id = \''+raw.id+'\' '
+        await conn.query(sql3)
         res.render('article', {
-            title: '文章详情',
+            title: raw.title,
             article: raw,
             aside: raw2
         })
