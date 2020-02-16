e2b5e2d13eef4cbece9003ceae400d37ebfc112b
hcyhehe <960696163@qq.com>
2020-02-16 16:24:06 +0800

    666

--- admin/src/js/global.js
+++ admin/src/js/global.js
@@ -3,8 +3,8 @@ const erp = erp || {}
 erp.API_SECRET_KEY = 'eblogVue.2666'
 erp.gaodeKey = '35982f18ea3de896092a670f25a11537'
 
-//const url = 'https://blog.allstackcoding.com/api'
-const url = 'http://localhost:8089/api'
+const url = 'https://blog.allstackcoding.com/api'
+//const url = 'http://localhost:8089/api'
 
 
 const bg = url
